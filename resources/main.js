c2b6630c664b8791c5680a53e522d56316e39b9e
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-10 11:42:10 +0300

    [finishes #163841869] removes inline CSS and JS

---
+++ resources/main.js
@@ -0,0 +1,18 @@
+
+document.getElementById("signup").onclick = function () {
+    location.href = "homepage.html";
+};
+document.getElementById("signin").onclick = function () {
+    location.href = "homepage.html";
+};
+document.getElementById("cancel").onclick = function () {
+    location.href = "signin.html";
+};
+document.getElementById("reset").onclick = function () {
+    location.href = "signin.html";
+};
+  
+         
+       
+        
+
